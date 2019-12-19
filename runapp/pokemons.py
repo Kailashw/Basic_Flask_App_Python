9bba7345090204909f317d8fb8ca37db09eded1a
Kailash CT <kailas@cloudthing.com>
2019-12-19 20:17:47 +0530

    added create and delete category API

--- runapp/pokemons.py
+++ runapp/pokemons.py
@@ -38,10 +38,10 @@ def fetch_pokemons():
         data = list(collection.find())
         # Return all the records as query string parameters are not available
         if len(data) > 0:
-            # Prepare response if the users are found
+            # Prepare response if the pokemons are found
             return dumps(data)
         else:
-            # Return empty array if no users are found
+            # Return empty array if no pokemons are found
             return jsonify([])
     except:
         # Error while trying to fetch the resource
@@ -51,19 +51,67 @@ def fetch_pokemons():
 @runapp.route("/api/v1/categories", methods=['GET'])
 def fetch_categories():
     """
-       Function to fetch the pokemons.
+       Function to fetch the categories.
        """
     try:
         data = list(categories_collection.find())
         # Return all the records as query string parameters are not available
         if len(data) > 0:
-            # Prepare response if the users are found
+            # Prepare response if the categories are found
             return dumps(data)
         else:
-            # Return empty array if no users are found
+            # Return empty array if no categories are found
             return jsonify([])
     except:
         # Error while trying to fetch the resource
+        # Add message for debugging purpose
+        return "", 500
+
+@runapp.route("/api/v1/categories", methods=['POST'])
+def create_category():
+    """
+       Function to create new category.
+       """
+    try:
+        # Create new categories
+        try:
+            body = ast.literal_eval(json.dumps(request.get_json()))
+        except:
+            # Bad request as request body is not available
+            # Add message for debugging purpose
+            return "", 400
+
+        record_created =  categories_collection.insert(body)
+
+        # Prepare the response
+        if isinstance(record_created, list):
+            # Return list of Id of the newly created item
+            return jsonify([str(v) for v in record_created]), 201
+        else:
+            # Return Id of the newly created item
+            return jsonify(str(record_created)), 201
+    except:
+        # Error while trying to create the resource
+        # Add message for debugging purpose
+        return "", 500
+
+@runapp.route("/api/v1/categories/<id>", methods=['DELETE'])
+def remove_category(id):
+    """
+       Function to remove the category.
+       """
+    try:
+        # Delete the category
+        delete_category = categories_collection.delete_one({"categoryId": int(id)})
+
+        if delete_category.deleted_count > 0 :
+            # Prepare the response
+            return "", 204
+        else:
+            # Resource Not found
+            return "", 404
+    except:
+        # Error while trying to delete the resource
         # Add message for debugging purpose
         return "", 500
 
